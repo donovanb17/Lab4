f77ec1052effb6aa70f6999bcf687c934f5a2f44
donovanb17 <donovanbantay@gmail.com>
2026-09-09 23:03:10 +0000

    initial attempt

---
+++ Gradebook.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+    int main(){
+        const int = 5;
+        int array[5];
+
+        void getScores(int scores[], int size){
+
+        do {
+            cout << "Please enter 5 test scores."
+            for (int i = 0; i < 5; i++){
+                cin >> scores << "Score" << i << ":" <<endl;
+            }
+            if (scores > 0 && scores > 100){
+                cout << "Invalid score. Please enter a value between 0 and 100." << endl;
+            }
+
+        }   while (scores <= 5);
+     
+        }
+
+
+
+
+
+
+    }
